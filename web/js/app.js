153d21750f0382813cfaf6e4d433c4c527da45c8
Ajmal Naha <ajmal@sayonetech.com>
2019-03-14 00:56:48 +0530

    new sections added

--- web/js/app.js
+++ web/js/app.js
@@ -46,6 +46,24 @@
        }
       
     });
+
+        // ========================================================================= //
+    //  Porfolio isotope and filter
+    // ========================================================================= // 
+    
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-grid-item'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+        $("#portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+
+        portfolioIsotope.isotope({
+            filter: $(this).data('filter')
+        });
+    });
+
     
   }(jQuery));
 
