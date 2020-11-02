0e0f47e995558cf955bf1d38a4fb0e4d462c44e7
ajmal.beco <ajmal.beco@gmail.com>
2020-11-02 22:41:09 +0530

    covert rgb to hex

--- assets/js/app.js
+++ assets/js/app.js
@@ -413,8 +413,16 @@
                         color = colorThief.getColor(item);
                     });
                 }
-                console.log(color);
+                hex(color)
             });
+
+            var hex = (item) => {
+                const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
+                    const hex = x.toString(16)
+                    return hex.length === 1 ? '0' + hex : hex
+                }).join('');
+                console.log(rgbToHex);
+            }
 
             // Make sure image is finished loading
             // if (img.complete) {
