28c4e17a55749310bbb3859368c7ce8f7b076b04
ajmal.beco <ajmal.beco@gmail.com>
2020-11-24 00:43:50 +0530

    make a wish updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -393,6 +393,145 @@
         handleUpload();
     }
 
+    PATH.makeAWishSec = () => {
+        var makeBtnClick = () => {
+            document.querySelector('.ps-make-wish').addEventListener('click', animateNavbar);
+            function animateNavbar() {
+                TweenMax.to('.make-canvas', 1, {
+                    opacity: 1,
+                    ease: Power3.ease,
+                    onComplete: function () {
+                        $('body').css({ 'overflow': 'hidden' });
+                        $('.make-a-wish-inner').attr('data-open', "true");
+                        TweenMax.to('.make-canvas', 1, {
+                            backgroundColor: '#30203c',
+                            ease: Power3.easeIn
+                        });
+                        TweenMax.to('.make-a-wish-inner', 1, {
+                            opacity: 1
+                        });
+                        TweenMax.to('.ps-form', 1, {
+                            opacity: 1,
+                            x: 0,
+                            ease: Power3.easeIn,
+                            onComplete: function () {
+                                TweenMax.staggerTo('.animate', 1, {
+                                    y: 0,
+                                    opacity: 1,
+                                    ease: Power3.easeIn,
+                                }, 0.2);
+                            }
+                        });
+                        TweenMax.to('.make-a-wish-inner .img-wrapper', 1, {
+                            opacity: 1,
+                            X: 0,
+                            ease: Power3.easeIn
+                        });
+                    }
+                });
+            }
+        }
+
+        makeBtnClick();
+
+        var makeBgCanvas = () => {
+            var Stars = {
+                canvas: null,
+                context: null,
+                circleArray: [],
+                colorArray: ['#2F2F82', '#85D6A6', '#F7D125', '#1f2e37', '#474848', '#542619', '#ead8cf', '#4c241f', '#d6b9b1', '#964a47'],
+                mouseDistance: 100,
+                radius: .5,
+                maxRadius: 1.5,
+                mouse: {
+                    x: undefined,
+                    y: undefined,
+                    down: false,
+                    move: false
+                },
+                init: function () {
+                    this.canvas = document.getElementById('canvas-make-bg');
+                    this.canvas.width = window.innerWidth;
+                    this.canvas.height = window.innerHeight;
+                    this.canvas.style.display = 'block';
+                    this.context = this.canvas.getContext('2d');
+
+                    window.addEventListener('mousemove', this.mouseMove);
+                    window.addEventListener('resize', this.resize);
+
+                    this.prepare();
+                    this.animate();
+                },
+                Circle: function (x, y, dx, dy, radius, fill) {
+                    this.x = x;
+                    this.y = y;
+                    this.dx = dx;
+                    this.dy = dy;
+                    this.radius = radius;
+                    this.minRadius = this.radius;
+
+                    this.draw = function () {
+                        Stars.context.beginPath();
+                        Stars.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+                        Stars.context.fillStyle = fill;
+                        Stars.context.fill();
+                    };
+                    this.update = function () {
+                        if (this.x + this.radius > Stars.canvas.width || this.x - this.radius < 0) this.dx = -this.dx;
+                        if (this.y + this.radius > Stars.canvas.height || this.y - this.radius < 0) this.dy = -this.dy;
+
+                        this.x += this.dx;
+                        this.y += this.dy;
+
+                        //  INTERACTIVITY
+                        if (Stars.mouse.x - this.x < Stars.mouseDistance && Stars.mouse.x - this.x > -Stars.mouseDistance && Stars.mouse.y - this.y < Stars.mouseDistance && Stars.mouse.y - this.y > -Stars.mouseDistance) {
+                            if (this.radius < Stars.maxRadius) this.radius += 1;
+                        } else if (this.radius > this.minRadius) {
+                            this.radius -= 1;
+                        }
+
+                        this.draw();
+                    };
+                },
+                prepare: function () {
+                    this.circleArray = [];
+
+                    for (var i = 0; i < 1200; i++) {
+                        var radius = Stars.radius;
+                        var x = Math.random() * (this.canvas.width - radius * 2) + radius;
+                        var y = Math.random() * (this.canvas.height - radius * 2) + radius;
+                        var dx = (Math.random() - 0.5) * 1.5;
+                        var dy = (Math.random() - 1) * 1.5;
+                        var fill = this.colorArray[Math.floor(Math.random() * this.colorArray.length)];
+
+                        this.circleArray.push(new this.Circle(x, y, dx, dy, radius, fill));
+                    }
+                },
+                animate: function () {
+                    requestAnimationFrame(Stars.animate);
+                    Stars.context.clearRect(0, 0, Stars.canvas.width, Stars.canvas.height);
+
+                    for (var i = 0; i < Stars.circleArray.length; i++) {
+                        var circle = Stars.circleArray[i];
+                        circle.update();
+                    }
+                },
+                mouseMove: function (event) {
+                    Stars.mouse.x = event.x;
+                    Stars.mouse.y = event.y;
+                },
+                resize: function () {
+                    Stars.canvas.width = window.innerWidth;
+                    Stars.canvas.height = window.innerHeight;
+                }
+            };
+            Stars.init();
+        }
+
+        makeBgCanvas();
+
+    }
+
     /* Document ready function */
     $(function () {
         PATH.backToTop();
@@ -406,6 +545,7 @@
         PATH.modalOpen();
         PATH.contactForm();
         PATH.blogBannerText();
+        PATH.makeAWishSec();
     });
 
     /* Window on load function */
